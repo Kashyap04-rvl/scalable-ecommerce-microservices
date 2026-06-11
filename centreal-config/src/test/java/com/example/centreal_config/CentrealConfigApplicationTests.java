83e35e9acc8fd4a05c561f4e6d43b9a0a696d786
Kashyap04-rvl <ravalkashyap04@gmail.com>
2026-06-11 16:08:34 +0530

    Added the central config application

---
+++ centreal-config/src/test/java/com/example/centreal_config/CentrealConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.centreal_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentrealConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
